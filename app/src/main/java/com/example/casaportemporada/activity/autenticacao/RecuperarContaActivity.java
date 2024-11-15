b2a1034b86ac829976d0d1735331ca68e6854e58
Júlio António <julioantonio1408@gmail.com>
2024-11-15 12:42:28 +0100

    processo de recuperação de senha implementado

--- app/src/main/java/com/example/casaportemporada/activity/autenticacao/RecuperarContaActivity.java
+++ app/src/main/java/com/example/casaportemporada/activity/autenticacao/RecuperarContaActivity.java
@@ -1,7 +1,11 @@
 package com.example.casaportemporada.activity.autenticacao;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.casaportemporada.R;
+import com.example.casaportemporada.helper.FirebaseHelper;
 
 public class RecuperarContaActivity extends AppCompatActivity {
+
+    private EditText edit_email;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +35,39 @@ public class RecuperarContaActivity extends AppCompatActivity {
         iniciaComponentes();
     }
 
+    public void validaDados(View view){
+        String email = edit_email.getText().toString();
+
+        if (!email.isEmpty()){
+            progressBar.setVisibility(View.VISIBLE);
+            recuperarSenha(email);
+        }else{
+            edit_email.requestFocus();
+            edit_email.setError("Informe seu e-mail");
+        }
+    }
+
+    private void recuperarSenha(String email){
+        FirebaseHelper.getAuth().sendPasswordResetEmail(email).addOnCompleteListener(
+                task -> {
+                    if (task.isSuccessful()){
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(this,"E-mail enviado com sucesso",Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressBar.setVisibility(View.GONE);
+                        String error = task.getException().getMessage();
+                        Toast.makeText(this,error,Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
     private void configCliques() {
         findViewById(R.id.ib_voltar).setOnClickListener(view -> finish());
     }
 
     private void iniciaComponentes(){
-        TextView text_titulo = findViewById(R.id.text_titulo);
-        text_titulo.setText("Recuperar Conta");
+        edit_email = findViewById(R.id.edit_email);
+        progressBar = findViewById(R.id.progressbar);
     }
 }
