7936e03b2c332a134597083bc63793398818e4bf
Júlio António <julioantonio1408@gmail.com>
2024-12-12 17:01:06 +0100

    refresh

--- app/src/main/java/com/example/casaportemporada/activity/MainActivity.java
+++ app/src/main/java/com/example/casaportemporada/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.casaportemporada.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.casaportemporada.R;
+import com.example.casaportemporada.activity.autenticacao.LoginActivity;
+import com.example.casaportemporada.helper.FirebaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton ib_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        iniciaComponents();
+        configCliques();
+    }
+
+    private void configCliques(){
+        ib_menu.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(this,ib_menu);
+            popupMenu.getMenuInflater().inflate(R.menu.menu_home, popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(menuItem -> {
+                if (menuItem.getItemId() == R.id.menu_filtrar){
+                    startActivity(new Intent(this, FiltrarAnunciosActivity.class));
+                } else if (menuItem.getItemId() == R.id.menu_meus_anuncios) {
+                    if (FirebaseHelper.getAutenticado()){
+                        startActivity(new Intent(this, MeusAnunciosActivity.class));
+                    }else{
+                        showAlertDialog();
+                    }
+                }else{
+                    if (FirebaseHelper.getAutenticado()){
+                        startActivity(new Intent(this, MinhaContaActivity.class));
+                    }else{
+                        showAlertDialog();
+                    }
+                }
+                return true;
+            });
+
+            popupMenu.show();
+        });
+    }
+
+    private void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Autenticação");
+        builder.setMessage("Não está autenticado, deseja fazer isso agora?");
+        builder.setCancelable(false);
+        builder.setNegativeButton("Não", (dialogInterface, i) -> dialogInterface.dismiss());
+        builder.setPositiveButton("Sim",(dialogInterface, i) ->{
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void iniciaComponents(){
+        ib_menu = findViewById(R.id.ib_menu);
     }
 }
